18a049d2c1fafb7861e7cffad56ae7139e69a907
Arthur Mühl <arthur.muehl@gesis.org>
2026-05-05 09:47:38 +0200

    Introducing methods hub files
    Uploading the methods hub tutorial for rwhatsapp and the files needed for the binder environment

---
+++ binder/install.R
@@ -0,0 +1,7 @@
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("lubridate")
+install.packages("tidyr")
+install.packages("tidytext")
+install.packages("stopwords")
+install.packages("rwhatsapp")
